05bafc66612ebbc9eee4ab8e3a6273f831efe048
Iusztin <p.e.iusztin@gmail.com>
2019-03-01 17:35:27 +0200

    Solve url problem. Delete existing UserEventReaction.

--- apps/events/views.py
+++ apps/events/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from apps.events import serializers
-from apps.events.models import Event
+from apps.events.models import Event, EventUserReaction
 
 
 class EventViewSet(mixins.ListModelMixin,
@@ -16,3 +16,15 @@ class EventViewSet(mixins.ListModelMixin,
 
 class EventUserReactionViewSet(mixins.CreateModelMixin, GenericViewSet):
     serializer_class = serializers.EventUserReactionSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get('user_id')
+        event_id = request.data.get('event_id')
+
+        try:
+            event_user_reaction = EventUserReaction.objects.get(user_id=user_id, event_id=event_id)
+            event_user_reaction.delete()
+        except EventUserReaction.DoesNotExist:
+            pass
+
+        return super().create(request, *args, **kwargs)
